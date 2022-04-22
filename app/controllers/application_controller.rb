fd36f6f6e49b0f13f7a78ba970b5a9563db2a120
Rabih Mteirek <rabihmteyrek@hotmail.com>
2022-04-22 17:20:21 -0400

    Fixed forms not working on replit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  # Added to make forms work with replit.com
+  # This disables authentication, only added for demo purposes
+  protect_from_forgery with: :null_session
 end
