2348aebd96943dd16d8d85ed20f4f91118fba34f
Rabih Mteirek <rabihmteyrek@hotmail.com>
2022-04-24 02:11:50 -0400

    Created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Generating Seeds...'
+
+puts 'Creating Items'
+item_names = %w[phone watch printer skateboard pencil book earphones]
+item_names.each do |item_name|
+  Item.create!(name: item_name.capitalize)
+end
+puts 'Done'
+
+
+puts 'Creating Wearhouses'
+locations = %w[Montreal Ottawa Toronto]
+locations.each_with_index do |location, index|
+  Warehouse.create!(name: "Warehouse #{index + 1}", location: location)
+end
+puts 'Done'
+
+puts 'Adding Items to different Wearhouses'
+warehouse_items = [
+  [item_names[0], locations[0], rand(1...50)],
+  [item_names[0], locations[1], rand(1...50)],
+  [item_names[1], locations[2], rand(1...50)],
+  [item_names[1], locations[0], rand(1...50)],
+  [item_names[2], locations[1], rand(1...50)],
+  [item_names[2], locations[2], rand(1...50)],
+  [item_names[3], locations[0], rand(1...50)],
+  [item_names[3], locations[1], rand(1...50)],
+  [item_names[4], locations[2], rand(1...50)],
+  [item_names[4], locations[0], rand(1...50)],
+  [item_names[5], locations[1], rand(1...50)],
+  [item_names[5], locations[2], rand(1...50)],
+  [item_names[6], locations[0], rand(1...50)],
+  [item_names[6], locations[1], rand(1...50)]
+]
+
+pp warehouse_items
+
+warehouse_items.each do |warehouse_item|
+  item = Item.find_by(name: warehouse_item[0].capitalize)
+  warehouse = Warehouse.find_by(location: warehouse_item[1])
+  quantity = warehouse_item[2]
+  WarehouseItem.create!(item: item, warehouse: warehouse, quantity: quantity)
+end
+puts 'Done'
+
+puts 'Finished!'
